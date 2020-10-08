706767bddcea0d7c4538c280b8dc254a1bbca042
AN <alecman95@gmail.com>
2020-10-09 00:11:06 +0200

    tt

--- Package.swift
+++ Package.swift
@@ -15,10 +15,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SDWebImage",
-            targets: ["SDWebImage"]),
-        .library(
-            name: "SDWebImageMapKit",
-            targets: ["SDWebImageMapKit"])
+            targets: ["SDWebImage"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
